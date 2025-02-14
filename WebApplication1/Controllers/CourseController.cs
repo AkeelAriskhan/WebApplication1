2c90b9d5582f3024dc361045a8cb3416b8e8e48d
AkeelAriskhan UCA <akeel@unicornconnectedapps.com>
2025-02-14 10:20:54 +0530

    first commit

---
+++ WebApplication1/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            List<Course> courses = new List<Course>
+            {
+                new Course { Id = 1, Name = "ASP.NET MVC", Instructor = "John Doe", DurationWeeks = 6 },
+                new Course { Id = 2, Name = "Angular", Instructor = "Jane Smith", DurationWeeks = 8 },
+                new Course { Id = 3, Name = "Flutter", Instructor = "Mike Johnson", DurationWeeks = 10 }
+            };
+
+            return View(courses);
+            
+        }
+    }
+}
---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Details()
+        {
+            // Create a list of students
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Alice", Age = 20, Course = "Computer Science" },
+                new Student { Id = 2, Name = "Bob", Age = 22, Course = "Mechanical Engineering" },
+                new Student { Id = 3, Name = "Charlie", Age = 21, Course = "Electrical Engineering" }
+            };
+            ViewData["Students"] = students;
+            return View();
+        }
+    }
+}
